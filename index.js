43872ade25cc3879e99b0fb8a3127c9ae157211b
Sean <skabe89@gmail.com>
2021-02-26 10:40:32 -0500

    commit tree e36fc44f09478373798a995c01824bf71c3479fd

--- index.js
+++ index.js
@@ -4,20 +4,39 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+    
+      for(let i = 0; i < collection.length; i ++){
+        callback(collection[i], i, collection)
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArr = []
+      for(let i = 0; i < collection.length; i ++){
+        newArr.push(callback(collection[i]))
+      }
+      return newArr
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      let total = acc
+      for(let i = 0; i < collection.length; i ++){
+        total = callback(total, collection[i], collection)
+      }
+      return total
     },
 
-    functions: function() {
-
+    find: function(collection, predicate) {
+      let i
+      for(let i = 0; i < collection.length; i++){
+        if (collection[i] === predicate){
+          return i = predicate
+          break
+        }
+      }
+      return i
     },
 
 
